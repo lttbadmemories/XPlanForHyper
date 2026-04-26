18f3e8d77affb2220563cbedbf1272e2d1c3d019
LTT_Tide <148367271+lttbadmemories@users.noreply.github.com>
2026-04-26 08:30:10 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,10 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
 }
